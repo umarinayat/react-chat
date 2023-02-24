06fc8c537cd1df882da85122632dbc379db63131
Umar Inayat <umarinayat111@gmail.com>
2023-02-24 05:12:13 +0500

    Added Logout

--- src/ChatHome.jsx
+++ src/ChatHome.jsx
@@ -1,5 +1,17 @@
-const ChatHome = () => {
-  return <div>ChatHome</div>;
+import { Link } from "react-router-dom";
+
+const ChatHome = ({ user, updateUser }) => {
+  const handleLogout = () => {
+    updateUser(null);
+  };
+  return (
+    <>
+      <h1>{user.displayName}</h1>
+      <button onClick={handleLogout}>
+        <Link to="/">Logout</Link>
+      </button>
+    </>
+  );
 };
 
 export default ChatHome;
--- src/Login.jsx
+++ src/Login.jsx
@@ -5,13 +5,11 @@ import {
   signInWithPopup,
 } from "firebase/auth";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import ChatHome from "./ChatHome";
 
+import ChatHome from "./ChatHome";
 import { app, auth } from "./firebase";
 
 const Login = () => {
-  const navigate = useNavigate();
   const [user, setUser] = useState(null);
 
   const loginWithGoogle = async () => {
@@ -20,19 +18,25 @@ const Login = () => {
     try {
       const result = await signInWithPopup(auth, provider);
       setUser(result.user);
-      navigate("/home");
     } catch (error) {
       console.error(error);
     }
   };
-  return (
-    <>
+
+  const updateUser = (newUser) => {
+    setUser(newUser);
+  };
+
+  const loginLogic = (
+    <div>
       <h1 style={{ textAlign: "center" }}>Best React Chat App</h1>
       <div className="signInDiv">
         <button onClick={loginWithGoogle}>Sign In</button>
       </div>
-      {user && <ChatHome user={user} />}
-    </>
+    </div>
+  );
+  return (
+    <>{user ? <ChatHome user={user} updateUser={updateUser} /> : loginLogic}</>
   );
 };
 
